a04fe6eae71323b7524177a761da70032b49af75
Carlana <mail@carlana.net>
2026-09-03 22:50:19 -0400

    Test DeepEqual

--- deep_equal.go
+++ deep_equal.go
@@ -8,8 +8,8 @@ import (
 func (be Tester) DeepEqual[T any](got, want T) Tester {
 	be.tb.Helper()
 	// Pass as pointers to get around the nil-interface problem
-	if !reflect.DeepEqual(&want, &got) {
-		be.fatalf("reflect.DeepEqual(%#v, %#v) == false", want, got)
+	if !reflect.DeepEqual(&got, &want) {
+		be.fatalf("reflect.DeepEqual(%#v, %#v) == false", got, want)
 	}
 	return be
 }
---
+++ deep_equal_example_test.go
@@ -0,0 +1,23 @@
+package assert_test
+
+import (
+	"github.com/earthboundkid/assert"
+)
+
+func ExampleTester_DeepEqual() {
+	// mock *testing.T for example purposes
+	be := assert.Relaxed(&mockingT{})
+
+	// good
+	m1 := map[int]bool{1: true, 2: false}
+	m2 := map[int]bool{1: true, 2: false}
+	be.DeepEqual(m1, m2)
+
+	// bad
+	var s1 []int
+	s2 := []int{}
+	be.DeepEqual(s1, s2) // DeepEqual is picky about nil vs. len 0
+
+	// Output:
+	// reflect.DeepEqual([]int(nil), []int{}) == false
+}
